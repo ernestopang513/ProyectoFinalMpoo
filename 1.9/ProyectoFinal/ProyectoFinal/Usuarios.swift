e2564516b364cdedaa85f73a4c0c0da545ae4c9f
ernestopang513 <32535374+ernestopang513@users.noreply.github.com>
2018-11-26 02:03:25 -0600

    Add files via upload

---
+++ 1.9/ProyectoFinal/ProyectoFinal/Usuarios.swift
@@ -0,0 +1,26 @@
+//
+//  Usuarios.swift
+//  ProyectoFinal
+//
+//  Created by Ernesto Pang on 11/2/18.
+//  Copyright © 2018 Ernesto Pang. All rights reserved.
+//
+
+import Foundation
+
+struct Usuarios: Codable{
+    var nameOfUsuario: String
+    var password: String
+    var estado: Bool
+    var school: String
+    var score: Double
+}
+/*
+ struct Questions{
+ var pregunta: String
+ var opciones: String
+ var resp: String
+ var Resultado: Bool
+ }
+ */
+
