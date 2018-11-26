c93cb4bfc472ddc4e55820a213b33cc714517a06
ernestopang513 <32535374+ernestopang513@users.noreply.github.com>
2018-11-26 02:00:43 -0600

    Proto 1
    El mas basico de los básicos

---
+++ 1.3/UsuariosPang/UsuariosPang/Usuarios.swift
@@ -0,0 +1,22 @@
+//
+//  Usuarios.swift
+//  UsuariosPang
+//
+//  Created by Ernesto Pang on 10/13/18.
+//  Copyright © 2018 Ernesto Pang. All rights reserved.
+//
+
+import Foundation
+
+struct Usuarios{
+    var nameOfUsuario: String
+    var password: String
+    var estado: Bool 
+}
+
+struct Questions{
+    var pregunta: String
+    var opciones: String
+    var resp: String
+    var Resultado: Bool
+}
